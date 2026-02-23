84a59a86d836d7d42e5a5ebf425f7db8e954ecb9
morgan <morgan.kpassi@gmail.com>
2026-02-23 21:44:32 +0100

    feat: auth user
    feat: auth login
    
    feat: logout update delete
    
    test: user crud & auth
    
    fix: delete guest & silent auth

--- pomo/database/migrations/1771845664529_create_users_table.ts
+++ pomo/database/migrations/1771845664529_create_users_table.ts
@@ -6,9 +6,11 @@ export default class extends BaseSchema {
   async up() {
     this.schema.createTable(this.tableName, (table) => {
       table.increments('id').notNullable()
-      table.string('full_name').nullable()
+      table.string('username').nullable()
       table.string('email', 254).notNullable().unique()
       table.string('password').notNullable()
+      table.string('first_name').notNullable()
+      table.string('last_name').notNullable()
 
       table.timestamp('created_at').notNullable()
       table.timestamp('updated_at').nullable()
--- pomo/start/routes.ts
+++ pomo/start/routes.ts
@@ -8,5 +8,12 @@
 */
 
 import router from '@adonisjs/core/services/router'
-router.on('/').renderInertia('home')
+const UsersController = () => import('#controllers/users_controller')
 
+router.on('/').renderInertia('home')
+router.get('users', [UsersController, 'index'])
+router.post('register', [UsersController, 'register'])
+router.put('users/:id', [UsersController, 'update'])
+router.delete('users/:id', [UsersController, 'destroy'])
+router.post('login', [UsersController, 'login'])
+router.post('logout', [UsersController, 'logout'])

d9c827d8c66be23092d0985d7d2fd49efdb6b7e8
morgan <morgan.kpassi@gmail.com>
2026-03-09 20:33:41 +0100

    feat: crud tasks

--- pomo/start/routes.ts
+++ pomo/start/routes.ts
@@ -8,12 +8,23 @@
 */
 
 import router from '@adonisjs/core/services/router'
-const UsersController = () => import('#controllers/users_controller')
+import UsersController from '#controllers/users_controller'
+import TasksController from '#controllers/tasks_controller'
 
 router.on('/').renderInertia('home')
-router.get('users', [UsersController, 'index'])
+
+// User routes
+router.get('api/users', [UsersController, 'index'])
 router.post('register', [UsersController, 'register'])
-router.put('users/:id', [UsersController, 'update'])
-router.delete('users/:id', [UsersController, 'destroy'])
+router.get('api/users/:id', [UsersController, 'show'])
+router.put('api/users/:id', [UsersController, 'update'])
+router.delete('api/users/:id', [UsersController, 'destroy'])
 router.post('login', [UsersController, 'login'])
 router.post('logout', [UsersController, 'logout'])
+
+// Task routes
+router.get('api/tasks', [TasksController, 'index'])
+router.post('api/tasks', [TasksController, 'store'])
+router.get('api/tasks/:id', [TasksController, 'show'])
+router.put('api/tasks/:id', [TasksController, 'update'])
+router.delete('api/tasks/:id', [TasksController, 'destroy'])
